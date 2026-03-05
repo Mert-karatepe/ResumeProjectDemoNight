98e13d2c9fa90a9a8b6fa90004c2c845987b5376
Mert-karatepe <79850131+Mert-karatepe@users.noreply.github.com>
2026-03-05 17:24:19 +0300

    add

--- ViewComponents/DefaultViewComponents/_DefaultServiceComponentPartial.cs
+++ ViewComponents/DefaultViewComponents/_DefaultServiceComponentPartial.cs
@@ -1,12 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ResumeProjectDemoNight.Context;
 
 namespace ResumeProjectDemoNight.ViewComponents.DefaultViewComponents
 {
     public class _DefaultServiceComponentPartial : ViewComponent
     {
+        private readonly ResumeContext _context;
+
+        public _DefaultServiceComponentPartial(ResumeContext context)
+        {
+            _context = context;
+        }
+
         public IViewComponentResult Invoke()
         {
-            return View();
+            var values= _context.Services.ToList();
+            return View(values);
         }
     }
 }

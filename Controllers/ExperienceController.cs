3561415f2072672ead590f94311d3fd6d9c3f012
Mert-karatepe <79850131+Mert-karatepe@users.noreply.github.com>
2026-03-07 13:50:52 +0300

    ...

---
+++ Controllers/ExperienceController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ResumeProjectDemoNight.Context;
+using ResumeProjectDemoNight.Entitites;
+
+namespace ResumeProjectDemoNight.Controllers
+{
+    public class ExperienceController : Controller
+    {
+        private readonly ResumeContext _context;
+
+        public ExperienceController(ResumeContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult ExperienceList()
+        {
+            var values = _context.Experiences.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult CreateExperience()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateExperience(Experience experience)
+        {
+            _context.Experiences.Add(experience);
+            _context.SaveChanges();
+            return RedirectToAction("ExperienceList");
+        }
+    }
+}
--- obj/Debug/net9.0/ResumeProjectDemoNight.AssemblyInfo.cs
+++ obj/Debug/net9.0/ResumeProjectDemoNight.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ResumeProjectDemoNight")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cb995d179e742a643f554d74b21278dd478de61")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66d7d4f228c094bf64260e8b755d4e9fee95a8c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ResumeProjectDemoNight")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ResumeProjectDemoNight")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
